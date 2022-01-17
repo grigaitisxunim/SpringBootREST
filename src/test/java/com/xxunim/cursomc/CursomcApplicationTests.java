e67469b8d458632c340b50220818e10ab3288e87
grigaitisxunim <junior.grigaitis98@hotmail.com>
2022-01-17 12:20:57 -0300

    REST testado

---
+++ src/test/java/com/xxunim/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxunim.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
